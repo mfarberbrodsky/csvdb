6ae0d3eccae8c4055866324e580d46c6e5625383
Maya Farber Brodsky <mayaf2003@gmail.com>
2018-12-18 19:18:33 +0200

    Finished select

---
+++ Schema.py
@@ -0,0 +1,22 @@
+import json
+
+class Schema:
+    def __init__(self, schema_filename):
+        self.load(schema_filename)
+
+    def load(self, schema_filename):
+        j = json.load(open(schema_filename))
+        self.fields = j["schema"]
+        self.name_to_index = {}
+        for i, f in enumerate(self.fields):
+            self.name_to_index[f["field"]] = i
+
+    def get_all_field_names(self):
+        return [field["field"] for field in self.fields]
+
+    def get_field_index(self, field_name):
+        return self.name_to_index[field_name]
+
+    def get_field_type(self, field_name):
+        i = self.get_field_index(field_name)
+        return self.fields[i]["type"]
--- create_table.py
+++ create_table.py
@@ -2,6 +2,7 @@ import json
 import sqltokenizer
 import os
 import csv
+from Schema import Schema
 
 
 # text =  r"""create table if not exists movies (title varchar, year int, duration int, score float);"""
@@ -80,7 +81,7 @@ def select(command):
 
     arg_list = []
     table_name = ""
-    outfile = ""
+    outfile = None
 
     while True:
         tok, val = tokenizer.next_token()
@@ -104,12 +105,36 @@ def select(command):
         if tok == sqltokenizer.SqlTokenKind.EOF:
             break
 
-    schema = []
-    with open(os.path.join(table_name, "table.json"), "r") as f:
-        schema = json.load(f)["schema"]
-
+    schema = Schema(os.path.join(table_name, "table.json"))
     if arg_list == ["*"]:
-        arg_list = [arg["field"] for arg in schema]
+        arg_list = schema.get_all_field_names()
+    arg_list_index = [schema.get_field_index(arg) for arg in arg_list]
+
+    with open(os.path.join(table_name, table_name + ".csv")) as table:
+        reader = csv.reader(table)
+        if outfile is None:
+            for row in reader:
+                row = [row[i] for i in range(len(row)) if i in arg_list_index]
+                print(",".join(row))
+        else:
+            with open(outfile, "w") as out_csv:
+                writer = csv.writer(out_csv)
+                for row in reader:
+                    row = [row[i] for i in range(len(row)) if i in arg_list_index]
+                    writer.writerow(row)
 
 
-select(input())
+def run_command(command):
+    command_name = command.split()[0]
+    if command_name == "create":
+        create_table(command)
+    elif command_name == "load":
+        load(command)
+    elif command_name == "select":
+        select(command)
+
+
+x = input()
+while x != "end":
+    run_command(x)
+    x = input()
